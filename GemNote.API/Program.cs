81304fb5451287cdd51965cf7fe4a4d3068a2353
Hoang Nguyen Vu <hoangnguyenvu1420@gmail.com>
2024-05-26 00:53:27 +0700

    Modify settings

--- GemNote.API/Program.cs
+++ GemNote.API/Program.cs
@@ -1,12 +1,109 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+using GemNote.API.Infrastructure.DataContext;
+using GemNote.API.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Add DbContext
+builder.Services.AddDbContext<GemNoteDbContext>(option =>
+{
+	option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// Add Identity
+builder.Services
+	.AddIdentity<AppUser, IdentityRole>()
+	.AddEntityFrameworkStores<GemNoteDbContext>()
+	.AddDefaultTokenProviders();
+
+// Configure Identity
+builder.Services.Configure<IdentityOptions>(options =>
+{
+	// Password settings
+	options.Password.RequireDigit = true;
+	options.Password.RequiredLength = 8;
+	options.Password.RequireNonAlphanumeric = false;
+	options.Password.RequireUppercase = false;
+	options.Password.RequireLowercase = false;
+
+	// Signin settings
+	options.SignIn.RequireConfirmedEmail = false;
+
+	// User settings
+	options.User.RequireUniqueEmail = true;
+});
+
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Add Authentication and JWT Bearer
+builder.Services.AddAuthentication(options =>
+{
+	options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+	options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+	options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(options =>
+{
+	options.TokenValidationParameters = new TokenValidationParameters
+	{
+		ValidateIssuer = true,
+		ValidateAudience = true,
+		ValidateLifetime = true,
+		ValidateIssuerSigningKey = true,
+		ValidIssuer = builder.Configuration["Jwt:Issuer"],
+		ValidAudience = builder.Configuration["Jwt:Audience"],
+		IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Secret"])),
+		ClockSkew = TimeSpan.Zero // Optional: Reduce token expiry time discrepancy
+	};
+});
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+	options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		Name = "Authorization",
+		In = ParameterLocation.Header,
+		Description = "Please enter your token with this format: ''Bearer YOUR_TOKEN''",
+		Type = SecuritySchemeType.ApiKey,
+		BearerFormat = "JWT",
+		Scheme = "Bearer"
+	});
+	options.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Name = "Bearer",
+				In = ParameterLocation.Header,
+				Reference = new OpenApiReference
+				{
+					Id = "Bearer",
+					Type = ReferenceType.SecurityScheme
+				}
+			},
+			new List<string>()
+		}
+	});
+});
+
+builder.Services.AddCors(options =>
+{
+	options.AddPolicy("AllowAll",
+		builder =>
+		{
+			builder
+				.AllowAnyOrigin()
+				.AllowAnyMethod()
+				.AllowAnyHeader();
+		});
+});
 
 var app = builder.Build();
 
@@ -19,8 +116,10 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseCors("AllowAll"); 
 
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapControllers();
 
