b467b7547bbe7a887152a914e0faa37537f14ac8
Hoang Nguyen Vu <hoangnguyenvu1420@gmail.com>
2024-05-26 00:53:02 +0700

    Add DbContext

---
+++ GemNote.API/Infrastructure/DataContext/GemNoteDbContext.cs
@@ -0,0 +1,22 @@
+﻿using GemNote.API.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace GemNote.API.Infrastructure.DataContext;
+
+public class GemNoteDbContext(DbContextOptions<GemNoteDbContext> options) : IdentityDbContext<AppUser>(options)
+{
+	public DbSet<AppUser> AppUsers { get; set; }
+	public DbSet<Category> Categories { get; set; }
+	public DbSet<Notebook> Notebooks { get; set; }
+	public DbSet<Section> Sections { get; set; }
+	public DbSet<Unit> Units { get; set; }
+	public DbSet<Flashcard> Flashcards { get; set; }
+	public DbSet<CardReviewSession> CardReviewSessions { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+
+	}
+}
