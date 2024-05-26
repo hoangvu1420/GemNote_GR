787c80e06a4c8dfee89e1e8e97d5c58accb04c86
Hoang Nguyen Vu <hoangnguyenvu1420@gmail.com>
2024-05-26 23:40:32 +0700

    Add ApiResponse

---
+++ GemNote.API/DTOs/ApiResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace GemNote.API.DTOs;
+
+public class ApiResponse
+{
+	public bool IsSucceed { get; set; }
+	public List<string>? ErrorMessages { get; set; }
+	public object? Data { get; set; }
+}
