b39bf2d48ea9f90ccaaa6ed134257e24d77003ae
Hoang Nguyen Vu <hoangnguyenvu1420@gmail.com>
2024-05-26 09:05:30 +0700

    Add auth service

---
+++ GemNote.API/Services/Contracts/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using GemNote.API.DTOs.AuthDtos;
+
+namespace GemNote.API.Services.Contracts;
+
+public interface IAuthService
+{
+	Task<AuthResponse> RegisterAsync(RegisterRequestDto request);
+	Task<LoginResponse> LoginAsync(LoginRequestDto request);
+	Task<RefreshTokenResponse> IssueRefreshTokenAsync(RefreshTokenRequestDto request);
+	Task<AuthResponse> AddToRoleAsync(AddToRoleDto request);
+}
