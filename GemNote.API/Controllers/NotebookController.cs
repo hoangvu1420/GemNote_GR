105b3bb12105faea3e5d1283da8cd90ec78109cb
Hoang Nguyen Vu <hoangnguyenvu1420@gmail.com>
2024-05-26 23:43:43 +0700

    Add methods to NotebookController

--- GemNote.API/Controllers/NotebookController.cs
+++ GemNote.API/Controllers/NotebookController.cs
@@ -1,10 +1,182 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Security.Claims;
+using GemNote.API.CustomFilters;
+using GemNote.API.DTOs;
+using GemNote.API.DTOs.NotebookDtos;
+using GemNote.API.Models;
+using GemNote.API.Services.Contracts;
+using GemNote.API.StaticDetails;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace GemNote.API.Controllers;
 
 [Route("api/notebooks")]
 [ApiController]
-public class NotebookController : ControllerBase
+[Authorize]
+public class NotebookController(INotebookService notebookService) : ControllerBase
 {
+	private ApiResponse _response = new();
+
+	[HttpGet]
+	[ResourceAuthorize(typeof(Notebook))] // Custom filter to authorize access to resources
+	[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+	[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+	[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status500InternalServerError)]
+	[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+	[ProducesResponseType(StatusCodes.Status403Forbidden)]
+	public async Task<IActionResult> GetNotebooksAsync([FromQuery] string? userId)
+	{
+		try
+		{
+			if (!string.IsNullOrEmpty(userId))
+			{
+				_response = await notebookService.GetNotebooksByUserIdAsync(userId);
+				if (!_response.IsSucceed)
+					return NotFound(_response);
+
+				return Ok(_response);
+			}
+
+			var userRoles = User.FindAll(ClaimTypes.Role).Select(c => c.Value).ToList();
+
+			if (!userRoles.Contains(UserRoles.Admin)) return Forbid();
+
+			_response = await notebookService.GetNotebooksAsync();
+			if (!_response.IsSucceed)
+				return NotFound(_response);
+
+			return Ok(_response);
+
+		}
+		catch (Exception e)
+		{
+			_response.IsSucceed = false;
+			_response.ErrorMessages = [e.Message];
+			return StatusCode(StatusCodes.Status500InternalServerError, _response);
+		}
+	}
+
+	[HttpGet("{notebookId}", Name = "GetNotebookById")]
+	[ResourceAuthorize(typeof(Notebook))] // Custom filter to authorize access to resources
+	[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+	[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+	[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status500InternalServerError)]
+	[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+	[ProducesResponseType(StatusCodes.Status403Forbidden)]
+	public async Task<IActionResult> GetNotebookById(int notebookId)
+	{
+		try
+		{
+			_response = await notebookService.GetNotebookByIdAsync(notebookId);
+			if (!_response.IsSucceed)
+				return NotFound(_response);
+
+			return Ok(_response);
+		}
+		catch (Exception e)
+		{
+			_response.IsSucceed = false;
+			_response.ErrorMessages = [e.Message];
+			return StatusCode(StatusCodes.Status500InternalServerError, _response);
+		}
+	}
+
+	[HttpPost]
+	[ResourceAuthorize(typeof(Notebook))] // Custom filter to authorize access to resources
+	[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status201Created)]
+	[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+	[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status500InternalServerError)]
+	[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+	[ProducesResponseType(StatusCodes.Status403Forbidden)]
+	public async Task<IActionResult> CreateNotebookAsync([FromBody] CreateNotebookDto notebook)
+	{
+		try
+		{
+			if (!ModelState.IsValid)
+			{
+				var errorMessages = ModelState.Values
+					.SelectMany(v => v.Errors)
+					.Select(e => e.ErrorMessage)
+					.ToList();
+
+				_response.IsSucceed = false;
+				_response.ErrorMessages = errorMessages;
+				return BadRequest(_response);
+			}
+
+			_response = await notebookService.CreateNotebookAsync(notebook);
+			var createdNotebookId = (_response.Data as NotebookDto)!.Id;
+
+			return CreatedAtRoute("GetNotebookById", new { notebookId = createdNotebookId }, _response);
+		}
+		catch (Exception e)
+		{
+			_response.IsSucceed = false;
+			_response.ErrorMessages = [e.Message];
+			return StatusCode(StatusCodes.Status500InternalServerError, _response);
+		}
+	}
+
+	[HttpPut("{notebookId}")]
+	[ResourceAuthorize(typeof(Notebook))] // Custom filter to authorize access to resources
+	[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+	[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status400BadRequest)]
+	[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+	[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status500InternalServerError)]
+	[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+	[ProducesResponseType(StatusCodes.Status403Forbidden)]
+	public async Task<IActionResult> UpdateNotebookAsync(int notebookId, [FromBody] UpdateNotebookDto notebookDto)
+	{
+		try
+		{
+			if (!ModelState.IsValid)
+			{
+				var errorMessages = ModelState.Values
+					.SelectMany(v => v.Errors)
+					.Select(e => e.ErrorMessage)
+					.ToList();
+
+				_response.IsSucceed = false;
+				_response.ErrorMessages = errorMessages;
+				return BadRequest(_response);
+			}
+
+			_response = await notebookService.UpdateNotebookAsync(notebookId, notebookDto);
+			if (!_response.IsSucceed)
+				return NotFound(_response);
+
+			return Ok(_response);
+		}
+		catch (Exception e)
+		{
+			_response.IsSucceed = false;
+			_response.ErrorMessages = [e.Message];
+			return StatusCode(StatusCodes.Status500InternalServerError, _response);
+		}
+	}
+
+	[HttpDelete("{notebookId}")]
+	[ResourceAuthorize(typeof(Notebook))] // Custom filter to authorize access to resources
+	[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status200OK)]
+	[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status404NotFound)]
+	[ProducesResponseType(typeof(ApiResponse), StatusCodes.Status500InternalServerError)]
+	[ProducesResponseType(StatusCodes.Status401Unauthorized)]
+	[ProducesResponseType(StatusCodes.Status403Forbidden)]
+	public async Task<IActionResult> DeleteNotebookAsync(int notebookId)
+	{
+		try
+		{
+			_response = await notebookService.DeleteNotebookAsync(notebookId);
+			if (!_response.IsSucceed)
+				return NotFound(_response);
+
+			return Ok(_response);
+		}
+		catch (Exception e)
+		{
+			_response.IsSucceed = false;
+			_response.ErrorMessages = [e.Message];
+			return StatusCode(StatusCodes.Status500InternalServerError, _response);
+		}
+	}
 }
