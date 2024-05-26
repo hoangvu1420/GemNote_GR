fdfcc97ef9970edc791dd7340586eae2436d16ac
Hoang Nguyen Vu <hoangnguyenvu1420@gmail.com>
2024-05-26 23:39:40 +0700

    Modify LoginRequestDto

--- GemNote.API/DTOs/AuthDtos/LoginRequestDto.cs
+++ GemNote.API/DTOs/AuthDtos/LoginRequestDto.cs
@@ -5,6 +5,7 @@ namespace GemNote.API.DTOs.AuthDtos;
 public class LoginRequestDto
 {
 	[Required(ErrorMessage = "Email is required")]
+	[EmailAddress(ErrorMessage = "Invalid email address")]
 	public string Email { get; init; }
 
 	[Required(ErrorMessage = "Password is required")]
