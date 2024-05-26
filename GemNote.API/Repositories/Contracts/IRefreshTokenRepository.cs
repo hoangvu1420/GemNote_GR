9fedd4af811727929cf025f1647bf7751a8abb9d
Hoang Nguyen Vu <hoangnguyenvu1420@gmail.com>
2024-05-26 09:05:19 +0700

    Add refresh token repository

---
+++ GemNote.API/Repositories/Contracts/IRefreshTokenRepository.cs
@@ -0,0 +1,8 @@
+﻿using GemNote.API.Models;
+
+namespace GemNote.API.Repositories.Contracts;
+
+public interface IRefreshTokenRepository : IRepository<RefreshToken>
+{
+	public Task SetRevoked(RefreshToken refreshToken);
+}
---
+++ GemNote.API/Repositories/Implementations/RefreshTokenRepository.cs
@@ -0,0 +1,18 @@
+﻿using GemNote.API.Infrastructure.DataContext;
+using GemNote.API.Models;
+using GemNote.API.Repositories.Contracts;
+using GemNote.API.Repositories.Implementations;
+
+namespace PetCareSystem.Repositories.Implementations;
+
+public class RefreshTokenRepository(GemNoteDbContext dbContext)
+	: Repository<RefreshToken>(dbContext), IRefreshTokenRepository
+{
+	private readonly GemNoteDbContext _dbContext1 = dbContext;
+
+	public async Task SetRevoked(RefreshToken refreshToken)
+	{
+		refreshToken.IsRevoked = true;
+		await _dbContext1.SaveChangesAsync();
+	}
+}
