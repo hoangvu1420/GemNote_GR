2e9e464476b7041e26a716a68066ec6e472400d7
Hoang Nguyen Vu <hoangnguyenvu1420@gmail.com>
2024-05-26 09:07:39 +0700

    Run refresh token migration

--- GemNote.API/Migrations/20240526020651_AddRefreshToken.Designer.cs
+++ GemNote.API/Migrations/20240526020651_AddRefreshToken.Designer.cs
@@ -4,6 +4,7 @@ using GemNote.API.Infrastructure.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GemNote.API.Migrations
 {
     [DbContext(typeof(GemNoteDbContext))]
-    partial class GemNoteDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240526020651_AddRefreshToken")]
+    partial class AddRefreshToken
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
