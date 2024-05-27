e63f02389838ae6bcb549b4c44003dd5c1d1e926
Hoang Nguyen Vu <hoangnguyenvu1420@gmail.com>
2024-05-27 22:44:47 +0700

    Modify LoginResponse

--- GemNote.API/DTOs/AuthDtos/LoginResponse.cs
+++ GemNote.API/DTOs/AuthDtos/LoginResponse.cs
@@ -3,10 +3,9 @@
 public class LoginResponse
 {
 	public bool IsSucceed { get; init; }
-	public List<string> ErrorMessages { get; init; } = [];
+	public List<string> ErrorMessages { get; init; }
 	public string Token { get; init; }
 	public DateTime ExpirationDate { get; init; }
 	public string RefreshToken { get; init; }
 	public UserDto UserInfo { get; init; }
-
 }
