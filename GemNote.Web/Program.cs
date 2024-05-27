848c4071f71c7e0b1c91c4b35a59a9e9fdaa4abb
Hoang Nguyen Vu <hoangnguyenvu1420@gmail.com>
2024-05-27 12:44:29 +0700

    Update front-end app

--- GemNote.Web/Program.cs
+++ GemNote.Web/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 namespace GemNote.Web;
 
@@ -12,6 +13,7 @@ public class Program
 		builder.RootComponents.Add<HeadOutlet>("head::after");
 
 		builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+		builder.Services.AddFluentUIComponents();
 
 		await builder.Build().RunAsync();
 	}
---
+++ GemNote.Web/ViewModels/Country.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GemNote.Web.ViewModels;
+
+public record Country(string Code, string Name)
+{
+	public string Flag => $"https://fluentui-blazor.net/_content/FluentUI.Demo.Shared/flags/{Code}.svg";
+
+	public MarkupString HtmlFlagAndName => (MarkupString)$"""
+	                                                      		<div style="display: flex; gap: 10px;">
+	                                                      			<img src="{Flag}" width: "20px" />
+	                                                      			<div>{Name}</div>
+	                                                      		</div>
+	                                                      """;
+
+	public static IEnumerable<Country> All
+	{
+		get
+		{
+			return new Country[]
+			{
+				new("us", "United States"),
+				new("ca", "Canada"),
+				new("mx", "Mexico"),
+				new("br", "Brazil"),
+				new("gb", "United Kingdom"),
+				new("de", "Germany"),
+				new("fr", "France"),
+				new("es", "Spain"),
+				new("it", "Italy"),
+				new("ru", "Russia"),
+				new("cn", "China"),
+				new("jp", "Japan"),
+				new("in", "India"),
+				new("au", "Australia"),
+				new("za", "South Africa"),
+				new("sa", "Saudi Arabia"),
+				new("ae", "United Arab Emirates"),
+				new("eg", "Egypt"),
+				new("ng", "Nigeria"),
+				new("ke", "Kenya"),
+				new("ar", "Argentina"),
+				new("cl", "Chile"),
+				new("co", "Colombia"),
+				new("pe", "Peru"),
+				new("ve", "Venezuela"),
+				new("id", "Indonesia"),
+				new("ph", "Philippines"),
+				new("vn", "Vietnam"),
+				new("th", "Thailand"),
+				new("my", "Malaysia"),
+				new("sg", "Singapore"),
+				new("kr", "South Korea"),
+				new("pk", "Pakistan"),
+				new("tr", "Turkey"),
+				new("ir", "Iran"),
+				new("iq", "Iraq")
+			}.OrderBy(c => c.Code);
+		}
+	}
+}
